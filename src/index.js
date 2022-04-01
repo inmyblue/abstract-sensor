fe3fb1954bfb75192a440207b42e877e13d41117
inmyblue <inmyblue0930@naver.com>
2022-04-02 08:00:54 +0900

    작성완료

--- src/index.js
+++ src/index.js
@@ -1,6 +1,42 @@
-class Sensor {}
+class Sensor {
+    constructor(id){
+        this.id = id
+        this.powerStatus = 'off'
+        this.status
+        this.reportingInterval = 10000
+    }
 
-class IotServer {}
+    turn(status){
+        if(this.powerStatus === 'on' && status === 'on') throw new Error("Error")
+        if(status === 'on') this.status = 'idle'
+        this.powerStatus = status
+
+        setTimeout(()=>this.status = 'sensingDistance', this.reportingInterval)
+        setTimeout(()=>this.status = 'reportingData', 500+this.reportingInterval)
+        setTimeout(()=>this.status = 'idle', 1500+this.reportingInterval)
+
+    }
+
+}
+
+class IotServer {
+    constructor(){
+        this.sensors
+    }
+    
+    start(sensor){
+        this.sensors=sensor
+    }
+
+    publish(action){
+        if(action.actionId === 'CHANGE_REPORTING_INTERVAL'){
+            this.sensors.map(v => {
+                if(v.id === action.deviceId && v.powerStatus === 'on') v.reportingInterval = action.payload
+            })
+        }
+        
+    }
+}
 
 module.exports = {
     Sensor,
